8dd2de2b0d66acf9bfe74d4d9aa74c2f7b5afe33
Kuromi-zzz <2764375914@qq.com>
2024-10-17 19:17:27 +0800

    Update NumberFeature.java

--- src/main/java/org/example/NumberFeature.java
+++ src/main/java/org/example/NumberFeature.java
@@ -4,11 +4,21 @@ import java.util.Scanner;
 
 public class NumberFeature {
     public static void main(String[] args) {
-        int inputNumber; // 输入的数字
-        int result = 0; // 输出结果
-        Scanner in = new Scanner(System.in);
-        inputNumber = in.nextInt();
-        // 你的代码逻辑
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        scanner.close();
+
+        int result = 0;
+        int position = 1;
+
+        while (num > 0) {
+            int digit = num % 10;
+            if (digit % 2 == position % 2) {
+                result += position;
+            }
+            num /= 10;
+            position *= 2;
+        }
 
         System.out.println(result);
     }
